fbd996243b7a5e5b1b60ade91f1eed94e6c07871
Sitthikorn Maneesut <sitthikornjuni@gmail.com>
2019-09-11 22:29:30 +0700

    Update v0.2
    - Finish logout dialog

--- src/app/_components/dialogs/logout/logout.component.ts
+++ src/app/_components/dialogs/logout/logout.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { NbDialogRef } from '@nebular/theme';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/_services';
 
 @Component({
   selector: 'app-logout',
@@ -11,8 +13,16 @@ export class LogoutComponent {
   @Input() title: string;
 
   constructor(
+    private authService: AuthService,
+    private router: Router,
     protected ref: NbDialogRef<LogoutComponent>
   ) { }
 
-
+  close() {
+    this.ref.close();
+  }
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/']);
+  }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
 import { NbDialogService } from '@nebular/theme';
-import { AuthService } from '../_services';
 import { LogoutComponent } from '../_components/dialogs/logout/logout.component';
 
 
@@ -15,23 +13,18 @@ import { LogoutComponent } from '../_components/dialogs/logout/logout.component'
 export class HomeComponent implements OnInit {
 
   constructor(
-    private authService: AuthService,
-    private router: Router,
     private dialogservice: NbDialogService
   ) { }
 
   ngOnInit() {
-    console.log(JSON.parse(localStorage.currentUser));
+    // console.log(JSON.parse(localStorage.currentUser));
   }
   opendialog() {
     this.dialogservice.open(LogoutComponent, {
+      closeOnBackdropClick: false,
       context: {
-        title: 'This is a title passed to the dialog component',
+        title: 'Are you logout ?',
       }
     });
-  }
-  logout() {
-    this.authService.logout();
-    this.router.navigate(['/']);
   }
 }
